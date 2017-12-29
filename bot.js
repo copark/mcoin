2ebcf89643397872f1f33294d8c96db4666cd820
Cheoloh.Park <cheoloh.park@gmail.com>
2017-12-29 21:20:02 +0900

    update

--- bot.js
+++ bot.js
@@ -15,18 +15,13 @@ else {
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
-var id = 0;
 var event = schedule.scheduleJob("*/10 * * * * *", function() {
     console.log('This runs every 10 seconds');
-    if (id != 0) { 
-        bot.sendMessage(id, 'every 10 seconds');
-    }
 });
 
 
 bot.onText(/^/, function (msg) {
   var name = msg.from.first_name;
-  id = msg.chat.id;
   bot.sendMessage(msg.chat.id, 'Hello, ' + name + '!').then(function () {
     // reply sent!
   });
